3eed5005d682b45b960913fa2932ebf8464554bc
Antonio Emmanuel C. Malabago <100856788+AntonioEmmanuelMalabago@users.noreply.github.com>
2023-10-31 22:13:04 +0800

    Added new files

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Computer Engineer", "Web3 Developer", "Blockchain Enthusiast", "IoT Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 500,
+    loop: true
+})
